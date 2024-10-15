00d2ab7f3e1dcf552b700938c3370c01193921d7
vlsk3104 <vlsk3104@gmail.com>
2024-10-16 00:34:46 +0900

    format space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
-	return (
-		<div className="p-2">
-			<Button>Primary</Button>
-			<Button variant="secondary">Secondary</Button>
-			<Button variant="destructive">Destructive</Button>
-			<Button variant="ghost">Ghost</Button>
-			<Button variant="muted">Muted</Button>
-			<Button variant="outline">Outline</Button>
-			<Button variant="tertiary">Tertiary</Button>
-			<div className="p-2">
-				<Input />
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-2">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
+      <div className="p-2">
+        <Input />
+      </div>
+    </div>
+  );
 }
--- src/features/test.tsx
+++ src/features/test.tsx
@@ -1,5 +1,5 @@
 const TestComponent = () => {
-	return <div className="text-red-500">TestComponent</div>;
+  return <div className="text-red-500">TestComponent</div>;
 };
 
 export default TestComponent;
